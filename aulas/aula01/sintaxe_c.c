32ee6c899a9e515a0462708298aac3045970096b
lucaslms9n <lucaslms9n@gmail.com>
2024-08-15 00:47:49 +0000

    calculadora 2 num

---
+++ aulas/aula01/sintaxe_c.c
@@ -0,0 +1,23 @@
+#include <stdio.h> //inclusão de biblioteca
+
+int main() { //função principal
+   // seu codigo aqui
+
+  // 1. entrada
+// levantar ingredientes
+// char, int, float
+// variaveis
+// leitura
+  int primeiro_numero;
+  int segundo_numero;
+  primeiro_numero = 5;
+  segundo_numero = 6;
+  // 2. processamento
+// calculo soma, subtracao, multiplicacao, divisao
+int soma = primeiro_numero + segundo_numero;
+  
+  // 3. saida
+// escrita, resultado 
+printf("A soma de %i com %i é %i\n", primeiro_numero, segundo_numero, soma);  
+    return 0; // return fim do programa
+}
